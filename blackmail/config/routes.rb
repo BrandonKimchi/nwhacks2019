1d5f5d451a02515f7c638656889b484e23ddc9b7
cabbageDev <fixedDjango@protonmail.com>
2019-01-26 18:29:41 -0800

    session edits

--- blackmail/config/routes.rb
+++ blackmail/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root 'blackmail#index'
   get 'sessions/new'
   get 'blackmail/index'
 
   get 'blackmail/register'
-  get 'blackmail/login'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'blackmail#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    'blackmail/login',   to: 'sessions#new'
+  post   'blackmail/login',   to: 'sessions#create'
+  delete 'blackmail/logout',  to: 'sessions#destroy'
+  resources :users
 end
