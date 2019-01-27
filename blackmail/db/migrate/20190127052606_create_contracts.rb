b0a2c3919d4afa4bb5796dd2bbfac51289172bc1
brandonk <btkim@uw.edu>
2019-01-27 12:39:20 -0800

    fix contract creation migrator

--- blackmail/db/migrate/20190127052606_create_contracts.rb
+++ blackmail/db/migrate/20190127052606_create_contracts.rb
@@ -1,12 +1,14 @@
 class CreateContracts < ActiveRecord::Migration[5.2]
   def change
     create_table :contracts do |t|
-      t.string :ownerUID
-      t.string :receiverUID
-      t.string :content
-      t.string :pwhash
-      t.integer :expriation
-      t.string :task
+      t.string: :ownerUID
+      t.string: :receiverUID
+      t.binary: :content
+      t.binary: :passhash
+      t.binary: :crypto_iv
+      t.integer: :expiration
+      t.string: :task
+      t.string: :contractid
 
       t.timestamps
     end
