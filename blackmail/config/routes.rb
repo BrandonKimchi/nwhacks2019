1b6f5180463d1358d3bc1d80b040811349cc9afb
brandonk <btkim@uw.edu>
2019-01-26 16:21:44 -0800

    homepage

--- blackmail/config/routes.rb
+++ blackmail/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'blackmail/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'blackmail#index'
 end
