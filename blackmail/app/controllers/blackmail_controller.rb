f8419f7df577041991367508c1779f01521c8030
brandonk <btkim@uw.edu>
2019-01-26 17:38:49 -0800

    extract params from form, hash password, ASSUMING PERFECT INPUT

--- blackmail/app/controllers/blackmail_controller.rb
+++ blackmail/app/controllers/blackmail_controller.rb
@@ -1,9 +1,19 @@
+require 'digest'
+
 class BlackmailController < ApplicationController
   def index
   end
   def login
   end
   def create_account
-    render plain: params[:username]
+    render plain: params[:account]
+    # puts params[:account]
+    account = params[:account]
+    passhash = Digest::SHA256.base64digest(account.require(:password))
+
+    # TODO Assuming no malformed input here, and not checking any input fields
+    # This will also allow duplicate entries for username, but it at least fucntional
+    @account = Account.create(username: account.require(:username),
+      passhash: passhash)
   end
 end
