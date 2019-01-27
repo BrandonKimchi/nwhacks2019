02d3afaa251794e89b956b4bdb6ba7100bd50fb5
brandonk <btkim@uw.edu>
2019-01-27 12:41:02 -0800

    aaa

--- blackmail/db/migrate/20190127052606_create_contracts.rb
+++ blackmail/db/migrate/20190127052606_create_contracts.rb
@@ -1,14 +1,14 @@
 class CreateContracts < ActiveRecord::Migration[5.2]
   def change
     create_table :contracts do |t|
-      t.string: :ownerUID
-      t.string: :receiverUID
-      t.binary: :content
-      t.binary: :passhash
-      t.binary: :crypto_iv
-      t.integer: :expiration
-      t.string: :task
-      t.string: :contractid
+      t.string :ownerUID
+      t.string :receiverUID
+      t.binary :content
+      t.binary :passhash
+      t.binary :crypto_iv
+      t.integer :expiration
+      t.string :task
+      t.string :contractid
 
       t.timestamps
     end
