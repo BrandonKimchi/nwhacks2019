72aa3d249e49f87106164dd929e47b6501cecc8a
cabbageDev <fixedDjango@protonmail.com>
2019-01-26 18:37:34 -0800

    Update routes.rb

--- blackmail/config/routes.rb
+++ blackmail/config/routes.rb
@@ -2,15 +2,8 @@ Rails.application.routes.draw do
   root 'blackmail#index'
   get 'sessions/new'
   get 'blackmail/index'
-
   get 'blackmail/register'
-<<<<<<< HEAD
-=======
   post 'blackmail/create_account'
-  get 'blackmail/login', to: 'sessions#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> master
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get    'blackmail/login',   to: 'sessions#new'
   post   'blackmail/login',   to: 'sessions#create'
